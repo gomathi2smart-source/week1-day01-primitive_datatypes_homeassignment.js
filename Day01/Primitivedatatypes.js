dda8537093d579627534a78bc7c77ff0700b41eb
gomathi2smart-source <gomathi2smart@gmail.com>
2026-03-03 13:06:13 +0530

    Day01 - Home assignemnt

---
+++ Day01/Primitivedatatypes.js
@@ -0,0 +1,32 @@
+// Primitive Data Types Assignment
+
+// a) String type
+var firstName = "Gomathi";
+
+// b) String type
+var companyName = "Testleaf";
+
+// c) Number type
+var mobileNumber = 9876543210;
+
+// d) Boolean type
+var isAutomation = true;
+
+// e) Undefined type (not assigned)
+var hasPlaywright;
+
+
+// Print values
+console.log("firstName value:", firstName);
+console.log("companyName value:", companyName);
+console.log("mobileNumber value:", mobileNumber);
+console.log("isAutomation value:", isAutomation);
+console.log("hasPlaywright value:", hasPlaywright);
+
+
+// Print data types using typeof
+console.log("firstName type:", typeof firstName);
+console.log("companyName type:", typeof companyName);
+console.log("mobileNumber type:", typeof mobileNumber);
+console.log("isAutomation type:", typeof isAutomation);
+console.log("hasPlaywright type:", typeof hasPlaywright);
